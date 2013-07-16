ab2ac37e779e105df9990d94dc8371eb58373dc0
Guilherme Budnieswski <guiiski@gmail.com>
2013-07-16 13:13:38 -0300

    Created project close

---
+++ close/footer.php
@@ -0,0 +1,18 @@
+
+	</div>
+	<!-- e# content -->
+
+	<div class="grid_12 footer">
+		<div class="social">
+			<div class="social-icon social-email"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-facebook"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-twitter"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-rss"><a href="#" target="_blank">Deviantart</a></div>
+		</div>	
+	</div>
+	<!-- e# footer -->
+
+</div>
+<!-- e# container_12 -->
+</body>
+</html>
---
+++ close/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>*-*</title>	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/icons.css">
+	<script type="text/javascript" src="js/script.js"></script>
+	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+<?php flush(); ?>
+<div class="container_12">
+	
+	<div class="grid_12 header">
+		<input type="text" name="" id="">
+	</div>
+	<!-- e# header -->
+  
+	<div class="grid_12 content">
