d1b16f3ae7623898bb2fc1738a62fb3ccc8f8bd6
Guilherme Budnieswski <guiiski@gmail.com>
2013-07-17 12:36:25 -0300

    new social-icons

--- close/footer.php
+++ close/footer.php
@@ -8,6 +8,11 @@
 			<div class="social-icon social-facebook"><a href="#" target="_blank">Deviantart</a></div>
 			<div class="social-icon social-twitter"><a href="#" target="_blank">Deviantart</a></div>
 			<div class="social-icon social-rss"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-mobile"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-apple"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-android"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-html5"><a href="#" target="_blank">Deviantart</a></div>
+			<div class="social-icon social-css"><a href="#" target="_blank">Deviantart</a></div>
 		</div>	
 	</div>
 	<!-- e# footer -->
