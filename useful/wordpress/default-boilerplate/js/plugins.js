38993d4fbf79989187fdab33f84bf38b1a4de9a9
Guilherme Budnieswski <guiiski@gmail.com>
2013-09-27 11:08:07 -0300

    implementado funcoes de cookies

--- useful/wordpress/default-boilerplate/js/plugins.js
+++ useful/wordpress/default-boilerplate/js/plugins.js
@@ -21,4 +21,51 @@
     }
 }());
 
+/* ==========================================================================
+   Functions
+   ========================================================================== */
+{
+    /*
+    * Cookie
+    */
+    {
+        function setCookie (c_name, value, exseg) {
+            var exdate = new Date();
+
+            exdate.setTime(exdate.getTime() + (exseg*1000));
+
+            var c_value = escape(value) + ((exseg==null) ? "" : "; expires="+exdate.toGMTString());
+
+            document.cookie = c_name + "=" + c_value+"; path=/";
+        }
+
+        function getCookie (c_name) {
+            var c_value = document.cookie,
+                c_start = c_value.indexOf(" " + c_name + "=");
+
+            if (c_start == -1) {
+              c_start = c_value.indexOf(c_name + "=");
+            }
+
+            if (c_start == -1) {
+              c_value = null;
+            } else {
+                c_start = c_value.indexOf("=", c_start) + 1;
+                var c_end = c_value.indexOf(";", c_start);
+
+                if (c_end == -1) {
+                    c_end = c_value.length;
+                }
+                c_value = unescape(c_value.substring(c_start,c_end));
+            }
+
+            return c_value;
+        }
+
+    } //end cookie
+} // end funcitons
+
+
+
+
 // Place any jQuery/helper plugins in here.
